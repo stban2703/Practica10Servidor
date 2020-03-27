189d830bbaeb95576fc0434efd4139a33926b97c
stban2703 <s.t.ban1401@gmail.com>
2020-03-27 15:55:37 -0500

    Primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.util.ArrayList;
+
+import comm.ComunicacionTCP;
+import comm.ComunicacionTCP.OnMessageListener;
+import processing.core.PApplet;
+
+public class Main extends PApplet implements OnMessageListener {
+
+	private ComunicacionTCP comunicacionTCP;
+	private ArrayList<Registro> listaRegistro;
+
+	public static void main(String[] args) {
+		PApplet.main("main.Main");
+	}
+
+	public void settings() {
+		size(500, 500);
+	}
+
+	public void setup() {
+		comunicacionTCP = new ComunicacionTCP();
+		listaRegistro = new ArrayList<Registro>();
+		comunicacionTCP.setObserver(this);
+		comunicacionTCP.esperarConexion();
+
+	}
+
+	public void draw() {
+		background(0);
+	}
+
+	@Override
+	public void onMessage(String mensaje) {
+		// TODO Auto-generated method stub
+		if (mensaje.startsWith("reg")) {
+			String[] datos = mensaje.split(",");
+			String nombre = datos[1];
+			String cedula = datos[2];
+
+			listaRegistro.add(new Registro(nombre, cedula));
+		}
+
+		if (mensaje.startsWith("list")) {
+			for (int i = 0; i < listaRegistro.size(); i++) {
+				String nombre = listaRegistro.get(i).getNombre();
+				String cedula = listaRegistro.get(i).getCedula();
+				comunicacionTCP.mandarMensaje(nombre + "," + cedula);
+			}
+		}
+
+	}
+}
---
+++ src/main/Registro.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Registro {
+	
+	String nombre;
+	String cedula;
+	
+	public Registro(String nombre, String cedula) {
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
+}
